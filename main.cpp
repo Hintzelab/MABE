a42dc2e9fe408cb7ca93991c2e8adc2fdaadfbbb
cgnitash <cgnitash@gmail.com>
2018-03-01 19:46:10 -0500

    modified pop init to read from .plf or settings file

--- main.cpp
+++ main.cpp
@@ -249,20 +249,10 @@ int main(int argc, const char *argv[]) {
 
     vector<shared_ptr<Organism>> population;
 
-    auto file_to_load= Global::initPopPL->get(PT);
-
+    auto file_to_load = Global::initPopPL->get(PT);
     Loader loader;
-    int population_size;
-    auto orgs_to_load =
-        load_value(file_to_load, population_size)
-            ? std::vector<std::pair<
-                  long, std::unordered_map<std::string, std::string>>>(
-                  population_size,
-                  make_pair(-2, std::unordered_map<std::string, std::string>()))
-
-            : loader.load_population(file_to_load);
-
-    population_size = orgs_to_load.size();
+    auto orgs_to_load = loader.load_population(file_to_load);
+    int population_size = orgs_to_load.size();
 
     // add population_size organisms which look like progenitor but could be
     // loaded from file
