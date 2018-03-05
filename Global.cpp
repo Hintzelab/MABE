c3d5e0fccbe73f2726c73576dc0288a8bbbf2c83
cgnitash <cgnitash@gmail.com>
2018-03-05 15:00:56 -0500

    removed using namespace std; from Gloabl.h/.cpp

--- Global.cpp
+++ Global.cpp
@@ -11,44 +11,44 @@
 #include "Global.h"
 
 // global variables that should be accessible to all
-shared_ptr<ParameterLink<int>> Global::randomSeedPL =
+std::shared_ptr<ParameterLink<int>> Global::randomSeedPL =
     Parameters::register_parameter("GLOBAL-randomSeed", 101,
                                    "seed for random number generator, if -1 "
                                    "random number generator will be seeded "
                                    "randomly");
-shared_ptr<ParameterLink<int>> Global::updatesPL =
+std::shared_ptr<ParameterLink<int>> Global::updatesPL =
     Parameters::register_parameter("GLOBAL-updates", 50,
                                    "how long the program will run");
-shared_ptr<ParameterLink<string>> Global::initPopPL =
+std::shared_ptr<ParameterLink<std::string>> Global::initPopPL =
     Parameters::register_parameter(
-        "GLOBAL-initPop", (string) "MASTER = default 100",
+        "GLOBAL-initPop", (std::string) "MASTER = default 100",
         "initial population to start MABE (if it's .plf syntax it will be \n"
         "parsed as such. If it's a file name with .plf that population loader "
         "file is parsed");
-shared_ptr<ParameterLink<string>> Global::modePL =
+std::shared_ptr<ParameterLink<std::string>> Global::modePL =
     Parameters::register_parameter(
-        "GLOBAL-mode", (string) "run",
+        "GLOBAL-mode", (std::string) "run",
         "mode to run MABE in [run,visualize,analyze]");
 
-shared_ptr<ParameterLink<int>> Global::maxLineLengthPL =
+std::shared_ptr<ParameterLink<int>> Global::maxLineLengthPL =
     Parameters::register_parameter("PARAMETER_FILES-maxLineLength", 80,
                                    "maximum line length");
-shared_ptr<ParameterLink<int>> Global::commentIndentPL =
+std::shared_ptr<ParameterLink<int>> Global::commentIndentPL =
     Parameters::register_parameter("PARAMETER_FILES-commentIndent", 30,
                                    "minimum space before comments");
 
-shared_ptr<ParameterLink<string>> Global::visualizePopulationFilePL =
+std::shared_ptr<ParameterLink<std::string>> Global::visualizePopulationFilePL =
     Parameters::register_parameter("VISUALIZATION_MODE-visualizePopulationFile",
-                                   (string) "genome_1000.csv",
+                                   (std::string) "genome_1000.csv",
                                    "which genome file do you wish to load");
-shared_ptr<ParameterLink<string>> Global::visualizeOrgIDPL =
+std::shared_ptr<ParameterLink<std::string>> Global::visualizeOrgIDPL =
     Parameters::register_parameter(
-        "VISUALIZATION_MODE-visualizeOrgIDs", (string) "[-1]",
+        "VISUALIZATION_MODE-visualizeOrgIDs", (std::string) "[-1]",
         "ID of Genome you would like to visualize. -1 last genome file, -2 all "
         "genomes in file (world must support group evaluate)");
 
-shared_ptr<ParameterLink<string>> Global::outputDirectoryPL =
-    Parameters::register_parameter("GLOBAL-outputDirectory", (string) "./",
+std::shared_ptr<ParameterLink<std::string>> Global::outputDirectoryPL =
+    Parameters::register_parameter("GLOBAL-outputDirectory", (std::string) "./",
                                    "where files will be written");
 
 // shared_ptr<ParameterLink<string>> Global::groupNameSpacesPL =
