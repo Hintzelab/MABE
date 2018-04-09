810f7221d373740e4c810e798875062fc88f33e5
cgnitash <cgnitash@gmail.com>
2018-04-08 21:07:57 -0400

    Added knockout Experiment feature

--- Analyze/Analyzer.cpp
+++ Analyze/Analyzer.cpp
@@ -3,6 +3,8 @@
 #include <memory>
 
 #include "Analyzer.h"
+//#include "../Group/Group.h"
+//#include "../modules.h"
 
 std::shared_ptr<ParameterLink<bool>> Analyzer::calculateKnockoutsPL =
     Parameters::register_parameter("ANALYZER-calculateKnockouts", false,
@@ -60,36 +62,66 @@ void Analyzer::fulfil_requirements(
 void Analyzer::knockoutExperiment(
     std::shared_ptr<AbstractWorld> world,
     std::map<std::string, std::shared_ptr<Group>> &groups) {
+  // this functionality tests gate knockout effects for Markov Brains
 
+  // this map is only needed since the world API needs a group
+  std::map<std::string, std::shared_ptr<Group>> mutated_groups;
+
+  // yank population out of the group that was passed in
   auto population = groups["root::"]->population;
-//  std::vector<shared_ptr<Organism>> mutated_population;
+
+  // for each brain(org) in the population
   for (auto org : population) {
 
-    world->evaluateSolo(org, 0, 0, 0);
+    // make a population containg just this org
+    std::vector<shared_ptr<Organism>> unmutated_population = {org};
+
+	// wrap this population up in a group
+    // for some reason this group needs the key to be exactly "root::"
+    mutated_groups["root::"] = std::make_shared<Group>(
+        unmutated_population, groups["root::"]->optimizer,
+        groups["root::"]->archivist);
+
+    // finally, we can evaluate the organism to get a baseline score
+    world->evaluate(mutated_groups, 0, 0, 0);
+
+    //	now yank the MarkovBrain out of the organism
     auto mb =
         std::make_shared<MarkovBrain>(dynamic_cast<MarkovBrain &>(*org->brain));
 
+    // for every brain with a single gate knockout
     for (auto b : mb->getAllSingleGateKnockouts()) {
 
+      // make a shell organism
       auto mutant = org->makeCopy(Parameters::root);
 
+      // wrap the brain in this organism
       mutant->brain = b;
 
-      world->evaluateSolo(mutant, 0, 0, 0);
+      // wrap the organism in a population
+      std::vector<shared_ptr<Organism>> mutated_population = {mutant};
+
+      // wrap the population in a group
+      mutated_groups["root::"] = std::make_shared<Group>(
+          mutated_population, groups["root::"]->optimizer,
+          groups["root::"]->archivist);
+
+      // finally  we can evaluate the knockout brain in the world
+      world->evaluate(mutated_groups, 0, 0, 0);
 
       auto ko_fitness = mutant->dataMap.getAverage("score");
 
+      //  accumulate all the knockout scores into the original organisms DataMap
       org->dataMap.append("KO_Scores", ko_fitness);
     }
-    
-	org->dataMap.setOutputBehavior("KO_Scores",
+
+    org->dataMap.setOutputBehavior("KO_Scores",
                                    DataMap::VAR + DataMap::AVE + DataMap::LIST);
     auto name = Analyzer::outputFileKnockoutNamePL->get();
     org->dataMap.writeToFile(name);
   }
 }
 
-  
 void
 Analyzer::  stateTransition(std::shared_ptr<AbstractWorld> world,
                      std::map<std::string, std::shared_ptr<Group>> &groups){
