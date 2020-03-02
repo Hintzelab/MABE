73f5e95644afac1bace8e282a258b80dcfc99730
Jory Schossau <joryschossau@gmail.com>
2020-03-02 18:04:17 -0500

    moved setup -> tools/, *_build binaries -> tools/

--- tools/setup.cmd
+++ tools/setup.cmd
@@ -5,22 +5,22 @@
 :; if [ "$(uname)" = 'Darwin' ]; then
 :;   echo "darwin"
 :;   # OSX
-:;   cp ${SCRIPT_DIR}/code/Utilities/osx_build ${SCRIPT_DIR}/mbuild
+:;   cp ${SCRIPT_DIR}/osx_build ${SCRIPT_DIR}/../mbuild
 :; elif [ "$(expr substr $(uname -s) 1 5)" = 'Linux' ]; then
 :;   # linux
-:;   cp ${SCRIPT_DIR}/code/Utilities/lin_build ${SCRIPT_DIR}/mbuild
+:;   cp ${SCRIPT_DIR}/lin_build ${SCRIPT_DIR}/../mbuild
 :; elif [ "$(expr substr $(uname -s) 1 10)" = 'MINGW32_NT' ]; then
 :;   # windows (MSYS/MinGW)
-:;   cp ${SCRIPT_DIR}/code/Utilities/win_build.exe ${SCRIPT_DIR}/mbuild.exe
+:;   cp ${SCRIPT_DIR}/win_build.exe ${SCRIPT_DIR}/../mbuild.exe
 :; elif [ "$(expr substr $(uname -s) 1 10)" = 'MINGW64_NT' ]; then
 :;   # windows (MSYS/MinGW)
-:;   cp ${SCRIPT_DIR}/code/Utilities/win_build.exe ${SCRIPT_DIR}/mbuild.exe
+:;   cp ${SCRIPT_DIR}/win_build.exe ${SCRIPT_DIR}/../mbuild.exe
 :; fi
 :; echo "created: mbuild"
 :; echo "Run ./mbuild to build MABE"
 :; exit
 @echo off
 SET THISDIR=%~dp0
-copy %THISDIR%\code\Utilities\win_build.exe %THISDIR%\mbuild.exe
+copy %THISDIR%\win_build.exe %THISDIR%\..\mbuild.exe
 echo created: mbuild.exe
 echo Run mbuild.exe to build MABE
