b41a801a74d5231b4dd1648df69ef7d6aa6e9c0c
Jory <joryschossau@gmail.com>
2020-03-03 15:35:13 -0500

    fixed path with space breaks setup script

--- tools/setup.cmd
+++ tools/setup.cmd
@@ -5,16 +5,16 @@
 :; if [ "$(uname)" = 'Darwin' ]; then
 :;   echo "darwin"
 :;   # OSX
-:;   cp ${SCRIPT_DIR}/osx_build ${SCRIPT_DIR}/../mbuild
+:;   cp "${SCRIPT_DIR}/osx_build" "${SCRIPT_DIR}/../mbuild"
 :; elif [ "$(expr substr $(uname -s) 1 5)" = 'Linux' ]; then
 :;   # linux
-:;   cp ${SCRIPT_DIR}/lin_build ${SCRIPT_DIR}/../mbuild
+:;   cp "${SCRIPT_DIR}/lin_build" "${SCRIPT_DIR}/../mbuild"
 :; elif [ "$(expr substr $(uname -s) 1 10)" = 'MINGW32_NT' ]; then
 :;   # windows (MSYS/MinGW)
-:;   cp ${SCRIPT_DIR}/win_build.exe ${SCRIPT_DIR}/../mbuild.exe
+:;   cp "${SCRIPT_DIR}/win_build.exe" "${SCRIPT_DIR}/../mbuild.exe"
 :; elif [ "$(expr substr $(uname -s) 1 10)" = 'MINGW64_NT' ]; then
 :;   # windows (MSYS/MinGW)
-:;   cp ${SCRIPT_DIR}/win_build.exe ${SCRIPT_DIR}/../mbuild.exe
+:;   cp "${SCRIPT_DIR}/win_build.exe" "${SCRIPT_DIR}/../mbuild.exe"
 :; fi
 :; echo "created: mbuild"
 :; echo "Run ./mbuild to build MABE"
