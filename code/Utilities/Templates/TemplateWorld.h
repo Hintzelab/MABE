a70d095be71892cb790a42d3ef16c28d2e4cc61b
Jory Schossau <joryschossau@gmail.com>
2021-06-14 20:06:08 -0400

    fixes whitespacing in TemplateWorld

--- code/Utilities/Templates/TemplateWorld.h
+++ code/Utilities/Templates/TemplateWorld.h
@@ -8,7 +8,7 @@
 //     to view the full license, visit:
 //         github.com/Hintzelab/MABE/wiki/License
 
-#pragma once					// directive to insure that this .h file is only included one time
+#pragma once    // directive to insure that this .h file is only included one time
 
 #include <World/AbstractWorld.h> // AbstractWorld defines all the basic function templates for worlds
 #include <string>
@@ -25,20 +25,20 @@ using std::to_string;
 class {{MODULE_NAME}}World : public AbstractWorld {
 
 public:
-	// parameters for group and brain namespaces
-	static shared_ptr<ParameterLink<int>> evaluationsPerGenerationPL;
+    // parameters for group and brain namespaces
+    static shared_ptr<ParameterLink<int>> evaluationsPerGenerationPL;
     
     // a local variable used for faster access to the ParameterLink value
     int evaluationsPerGeneration;
-	
+    
     std::string groupName = "root::";
     std::string brainName = "root::";
     
     {{MODULE_NAME}}World(shared_ptr<ParametersTable> PT);
-	virtual ~{{MODULE_NAME}}World() = default;
+    virtual ~{{MODULE_NAME}}World() = default;
 
-	virtual auto evaluate(map<string, shared_ptr<Group>>& /*groups*/, int /*analyze*/, int /*visualize*/, int /*debug*/) -> void override;
+    virtual auto evaluate(map<string, shared_ptr<Group>>& /*groups*/, int /*analyze*/, int /*visualize*/, int /*debug*/) -> void override;
 
-	virtual auto requiredGroups() -> unordered_map<string,unordered_set<string>> override;
+    virtual auto requiredGroups() -> unordered_map<string,unordered_set<string>> override;
 };
 
